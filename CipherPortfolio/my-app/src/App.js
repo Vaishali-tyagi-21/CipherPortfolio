a45fb4f863028e43cbe7c9b2630e0a51402e683c
Vaishali Tyagi <109030244+Vaishali-tyagi-21@users.noreply.github.com>
2023-01-30 20:11:38 +0530

    Add files via upload

--- CipherPortfolio/my-app/src/App.js
+++ CipherPortfolio/my-app/src/App.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './NavBar';
-import styled from 'styled-components';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ComponentTwo from './ComponentTwo';
-
-
-const Container = styled.div`
-background-color: crimson;
-`;
-
-
+import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
+import Services from "./components/Services/Services";
+import "./App.css";
+import Experience from "./components/Experience/Experience";
+import Works from "./components/Works/Works";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonial from "./components/Testimonials/Testimonial";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import { useContext } from "react";
+import { themeContext } from "./Context";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <NavBar/>,
-    },
-    {
-      path:'/about',
-      element:<ComponentTwo/>
-    }
-  ]);
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <Container >
-      <RouterProvider router={router} />
-    </Container>
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
-
 
 export default App;
